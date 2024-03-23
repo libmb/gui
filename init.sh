1d115a6ec541954845f18968de3e2bf3812fd186
맹주영 <mjy9088@naver.com>
2024-03-23 16:55:51 +0900

    make venv optional

--- init.sh
+++ init.sh
@@ -11,13 +11,6 @@ cmake --build tmp/builddir --target install --config Release --parallel
 mv tmp/builddir/external ./tmp
 rm -rf tmp/builddir
 
-# glad
-python3 -m venv tmp/venv
-. tmp/venv/bin/activate
-(cd submodules/glad && pip3 install -r requirements.txt && python3 -m glad --out-path=../.. --reproducible --api=gl:core=3.3 c)
-deactivate
-rm -rf tmp/venv
-
 # core
 make -C submodules/core
 cp -r submodules/core/include/* tmp/external/include
@@ -35,53 +28,6 @@ make -C submodules/colorizer_basic
 cp -r submodules/colorizer_basic/include/* tmp/external/include
 cp submodules/colorizer_basic/libmb_colorizer_basic.a tmp/external/lib
 make -C submodules/colorizer_basic clean
-
-# compile_commands.json
-echo "[
-  {
-    \"directory\": \"$(pwd)\",
-    \"file\": \"src/lib.c\",
-    \"output\": \"/dev/null\",
-    \"arguments\": [
-      \"clang\",
-      \"-xc\",
-      \"src/main.c\",
-      \"-o\",
-      \"/dev/null\",
-      \"-I\",
-      \"external/include\",
-      \"-I\",
-      \"include\",
-      \"-Werror\",
-      \"-pedantic\",
-      \"-std=c99\",
-      \"-Wall\",
-      \"-Wextra\",
-      \"-Wpedantic\",
-      \"-Wcast-qual\",
-      \"-Wconversion\",
-      \"-Wdouble-promotion\",
-      \"-Wduplicated-branches\",
-      \"-Wduplicated-cond\",
-      \"-Wfloat-equal\",
-      \"-Wformat=2\",
-      \"-Wformat-signedness\",
-      \"-Winit-self\",
-      \"-Wlogical-op\",
-      \"-Wmissing-declarations\",
-      \"-Wmissing-prototypes\",
-      \"-Wpadded\",
-      \"-Wshadow\",
-      \"-Wstrict-prototypes\",
-      \"-Wswitch-default\",
-      \"-Wswitch-enum\",
-      \"-Wundef\",
-      \"-Wunused-macros\",
-      \"-Wwrite-strings\",
-      \"-c\"
-    ]
-  }
-]" > compile_commands.json
 
 rm -rf external
 mv tmp/external .
